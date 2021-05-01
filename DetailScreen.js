cf3dbf2c40f960a3d93e60d5893fb6734bd63f61
K1744680 <k1744680@kingston.ac.uk>
2021-05-01 18:31:58 +0500

    @expo vector icons list added

--- DetailScreen.js
+++ DetailScreen.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import Ionicons from 'react-native-vector-icons/Ionicons'
+import { Ionicons } from '@expo/vector-icons'; 
 import Header from './Header'
 
 const DetailScreen = ({ navigation, route }) => {
